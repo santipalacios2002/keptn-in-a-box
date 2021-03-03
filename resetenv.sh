88cc83f6ca571d6df45ad5731239e23638e4fc7e
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-03 15:34:24 -0500

    Update resetenv.sh

--- resetenv.sh
+++ resetenv.sh
@@ -5,7 +5,7 @@
 #
 KEPTN_IN_A_BOX_REPO="https://github.com/jyarb-keptn/keptn-in-a-box.git"
 KIAB_RELEASE="release-0.8"
-KIAB_FILE_REPO="https://raw.githubusercontent.com/dthotday-performance/keptn-in-a-box/${KIAB_RELEASE}/keptn-in-a-box.sh"
+KIAB_FILE_REPO="https://raw.githubusercontent.com/jyarb-keptn/keptn-in-a-box/${KIAB_RELEASE}/keptn-in-a-box.sh"
 
 sudo snap remove microk8s --purge
 
