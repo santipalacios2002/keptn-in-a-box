199f3d50df0d95c99be06e35c24fdd709f06bcd0
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-03 18:11:47 -0500

    8.0 changes

--- resources/catalog/deploy_catalog_3.sh
+++ resources/catalog/deploy_catalog_3.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 
 # Trigger the deployment
-keptn send event new-artifact --project=keptnorders --service=catalog --image=docker.io/dtdemos/dt-orders-catalog-service --tag=1
+keptn trigger delivery --project=keptnorders --service=catalog --image=docker.io/dtdemos/dt-orders-catalog-service --tag=1
 
 # Trigger the deployment
-keptn send event new-artifact --project=keptnorders --service=order --image=docker.io/dtdemos/dt-orders-order-service --tag=3
+keptn trigger delivery --project=keptnorders --service=order --image=docker.io/dtdemos/dt-orders-order-service --tag=3
 
 # Trigger the deployment
-keptn send event new-artifact --project=keptnorders --service=frontend --image=docker.io/dtdemos/dt-orders-frontend --tag=1
+keptn trigger delivery --project=keptnorders --service=frontend --image=docker.io/dtdemos/dt-orders-frontend --tag=1
 
 # Trigger the deployment
-keptn send event new-artifact --project=keptnorders --service=customer --image=docker.io/dtdemos/dt-orders-customer-service --tag=3
+keptn trigger delivery --project=keptnorders --service=customer --image=docker.io/dtdemos/dt-orders-customer-service --tag=3
