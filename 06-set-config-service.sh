78aa28c62f28d2a17a5a96944ccdc72f3c21145d
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-04 11:32:53 -0500

    Update 06-set-config-service.sh

--- 06-set-config-service.sh
+++ 06-set-config-service.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # use to change the jmeter service.
 
-ALT_JMETER_SERVICE_BRANCH="release-0.7.3-patch1"
+ALT_JMETER_SERVICE_BRANCH="release-0.8.0-alpha"
 
 # use for host aliases
 sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/${ALT_JMETER_SERVICE_BRANCH}/configuration-service/deploy/service.yaml -n keptn --record
 
 # use for host aliases
-sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/${ALT_JMETER_SERVICE_BRANCH}/jmeter-service/deploy/service.yaml -n keptn --record
+#sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/${ALT_JMETER_SERVICE_BRANCH}/jmeter-service/deploy/service.yaml -n keptn --record
 # original to KIAB
 # sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/feature/2552/jmeterextensionskeptn072/jmeter-service/deploy/service.yaml -n keptn --record
 
 # use for host aliases
-sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/${ALT_JMETER_SERVICE_BRANCH}/bridge/deploy/bridge.yaml -n keptn --record
+#sudo kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/${ALT_JMETER_SERVICE_BRANCH}/bridge/deploy/bridge.yaml -n keptn --record
 
 sleep 10
 
