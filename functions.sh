4e0d40cbcc182e89c971137f9919ac467ffaf633
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-04 09:16:49 -0500

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -611,7 +611,7 @@ jmeterService() {
   #bashas "kubectl -n keptn set image deployment/jmeter-service jmeter-service=keptncontrib/jmeter-extended-service:0.2.0 --record"
   ## User for jmeter mint
   #bashas "kubectl apply -f https://raw.githubusercontent.com/keptn/keptn/${ALT_JMETER_SERVICE_BRANCH}/jmeter-service/deploy/service.yaml -n keptn --record"
-  bashas "kubectl apply -f https://raw.githubusercontent.com/keptn/keptn/release-0.8.0-rc1/jmeter-service/deploy/service.yaml -n keptn --record"
+  bashas "kubectl apply -f https://raw.githubusercontent.com/dthotday-performance/keptn/release-0.8.0-alpha/jmeter-service/deploy/service.yaml -n keptn --record"
   fi
 }
 
