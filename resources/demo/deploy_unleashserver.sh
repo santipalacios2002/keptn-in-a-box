409af3d463fbed30e7a8b10a95ddf441e456ff1b
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-08 10:53:21 -0500

    Update deploy_unleashserver.sh

--- resources/demo/deploy_unleashserver.sh
+++ resources/demo/deploy_unleashserver.sh
@@ -7,7 +7,7 @@ if [[ -d "unleash" ]]; then
     keptn onboard service unleash-db --project=unleash --chart=./unleash-db
     keptn onboard service unleash --project=unleash --chart=./unleash
     keptn trigger delivery --project=unleash --service=unleash-db --image=postgres:10.4
-    keptn trigger delivery --project=unleash --service=unleash --image=docker.io/keptnexamples/unleash:1.0.0
+    keptn trigger delivery --project=unleash --service=unleash --image=docker.io/keptnexamples/unleash:1.0.1
 
     #keptn update project unleash --git-user="GIT_USER" --git-token="GIT_TOKEN" --git-remote-url="GIT_REMOTE_URL"
     # Configure Keptn
