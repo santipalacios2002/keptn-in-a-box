6843497a49ad471b1dfb22d3df156531ba472bf4
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-15 17:46:30 -0400

    Update deploy_carts_0.sh

--- resources/demo/deploy_carts_0.sh
+++ resources/demo/deploy_carts_0.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
 # Trigger the deployment
-keptn send event new-artifact --project=sockshop --service=carts-db --image=docker.io/mongo --tag=4.2.2
+keptn trigger delivery --project=sockshop --service=carts-db --image=docker.io/mongo --tag=4.2.2
 
 # Trigger the deployment
-keptn send event new-artifact --project=sockshop --service=carts --image=docker.io/keptnexamples/carts --tag=0.11.1
+keptn trigger delivery --project=sockshop --service=carts --image=docker.io/keptnexamples/carts --tag=0.11.1
 
