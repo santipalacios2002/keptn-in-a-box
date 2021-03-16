e6c601905f7ac988e480c54acad04d50bcd6e9ed
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-16 15:46:51 -0400

    Update 07-keptn0.8.0-patchs.sh

--- 07-keptn0.8.0-patchs.sh
+++ 07-keptn0.8.0-patchs.sh
@@ -6,5 +6,10 @@ KEPTN_IN_A_BOX_DIR="~/keptn-in-a-box"
 cd ~/keptn-in-a-box/resources/gitea
 ./update-git-keptn-post-flight.sh
 
+#cd ~/examples/onboarding-carts
+#keptn add-resource --project=sockshop --resource=sli-config-dynatrace.yaml --resourceUri=dynatrace/sli.yaml
+
+cd ~/keptn-in-a-box/resources/demo
+./deploy_carts_0.sh
 
 #keptn configure monitoring dynatrace --project=performance
