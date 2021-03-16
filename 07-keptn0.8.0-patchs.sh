0eef0d58de4ca2903ad240fc7e541772cba9afbe
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-16 15:51:10 -0400

    Update 07-keptn0.8.0-patchs.sh

--- 07-keptn0.8.0-patchs.sh
+++ 07-keptn0.8.0-patchs.sh
@@ -2,7 +2,7 @@
 # perform steps for keptn 0.8.0
 KEPTN_IN_A_BOX_DIR="~/keptn-in-a-box"
 
-cd ~/keptn-in-a-box/resources/demo
+cd ~/examples/onboarding-carts
 keptn create project sockshop --shipyard=./shipyard.yaml
 
 cd ~/keptn-in-a-box/resources/gitea
