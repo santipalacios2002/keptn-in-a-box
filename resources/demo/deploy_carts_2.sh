36ca7eb97cc640df73826e9e9d270e63ed86dfa3
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-17 14:42:53 -0400

    Update deploy_carts_2.sh

--- resources/demo/deploy_carts_2.sh
+++ resources/demo/deploy_carts_2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
-keptn trigger delivery --project=sockshop --service=carts --image=docker.io/keptnexamples/carts --tag=0.11.2 --labels=creator=cli,build=02
+keptn trigger delivery --project=sockshop --service=carts --image=docker.io/keptnexamples/carts --tag=0.12.2 --labels=creator=cli,build=02
 
