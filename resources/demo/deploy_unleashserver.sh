780d0a0810b3e1bf12c069a11151a06337d61f1d
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-17 17:04:34 -0400

    use for testing

--- resources/demo/deploy_unleashserver.sh
+++ resources/demo/deploy_unleashserver.sh
@@ -4,7 +4,8 @@
 if [[ -d "unleash" ]]; then
 
     UNLEASH_TOKEN=$(echo -n keptn:keptn | base64)
-    UNLEASH_BASE_URL=$(echo http://unleash.unleash-dev.$(kubectl -n keptn get ingress api-keptn-ingress -ojsonpath='{.spec.rules[0].host}'))
+    #UNLEASH_BASE_URL=$(echo http://unleash.unleash-dev.$(kubectl -n keptn get ingress api-keptn-ingress -ojsonpath='{.spec.rules[0].host}'))
+    UNLEASH_BASE_URL=$(echo http://unleash.unleash-dev.kiab.pcjeffint.com)
     
     # The context for this script needs to be in examples/unleash-server
     keptn create project unleash --shipyard=./shipyard.yaml
