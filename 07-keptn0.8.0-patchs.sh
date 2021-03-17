f03c904db51515a3910cae5d475d7bb6554a2af9
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-16 20:10:26 -0400

    Update 07-keptn0.8.0-patchs.sh

--- 07-keptn0.8.0-patchs.sh
+++ 07-keptn0.8.0-patchs.sh
@@ -4,6 +4,8 @@ KEPTN_IN_A_BOX_DIR="~/keptn-in-a-box"
 
 cd ~/examples/onboarding-carts
 keptn add-resource --project=sockshop --resource=shipyard.yaml --resourceUri=shipyard.yaml
+#cd ~/keptn-in-a-box/resources/demo
+#keptn add-resource --project=sockshop --stage=dev --service=carts-db --resource=slo-carts-db.yaml --resourceUri=slo.yaml
 
 cd ~/keptn-in-a-box/resources/gitea
 ./update-git-keptn-post-flight.sh
