d374b36aad3b9ec8366321ed3ac547e466bf4ef8
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-17 20:47:18 -0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -9,7 +9,7 @@
 #      ----- Components Versions -----             #
 # ==================================================
 KIAB_RELEASE="release-0.8pre"
-ISTIO_VERSION=1.9.1
+ISTIO_VERSION=1.5.10
 
 CERTMANAGER_VERSION=0.14.0
 # https://github.com/keptn/keptn
