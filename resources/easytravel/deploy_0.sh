50ca0a6572d2b342ee4bc78e7ebd86d9c080eada
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-25 14:52:53 -0400

    Update deploy_0.sh

--- resources/easytravel/deploy_0.sh
+++ resources/easytravel/deploy_0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 # Trigger the deployment
-keptn trigger delivery --project=easytravel --service=easytravel-mongodb --image=docker.io/dynatrace/easytravel-mongodb --tag=2.0.0.3156 --labels=creator=cli
+keptn trigger delivery --project=easytravel --service=easytravel-mongodb --image=docker.io/dynatrace/easytravel-mongodb --tag=2.0.0.3156 --sequence=delivery-direct --labels=creator=cli
 sleep 30
 # Trigger the deployment
 keptn trigger delivery --project=easytravel --service=easytravel-backend --image=docker.io/dynatrace/easytravel-backend --tag=2.0.0.3256 --labels=creator=cli
