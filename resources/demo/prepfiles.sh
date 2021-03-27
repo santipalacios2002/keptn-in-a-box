a4292a34a28d5168d23b39df59d4ee0baa4fb70e
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-27 14:39:02 -0400

    Update prepfiles.sh

--- resources/demo/prepfiles.sh
+++ resources/demo/prepfiles.sh
@@ -6,5 +6,5 @@ KEPTN_EXAMPLES_DIR="examples"
 cd ~
 
 #move files for jmeter
-cp /home/dynatrace/$KEPTN_PREP_DIR/demo_onbording/dev/carts/jmeter/load.jmx $KEPTN_EXAMPLES_DIR/onboarding-carts/jmeter/load.jmx
-cp /home/dynatrace/$KEPTN_PREP_DIR/demo_onbording/dev/carts/jmeter/basiccheck.jmx $KEPTN_EXAMPLES_DIR/onboarding-carts/jmeter/basiccheck.jmx
+cp /home/dynatrace/$KEPTN_PREP_DIR/demo_onbording/dev/carts/jmeter/load.jmx /home/dynatrace/$KEPTN_EXAMPLES_DIR/onboarding-carts/jmeter/load.jmx
+cp /home/dynatrace/$KEPTN_PREP_DIR/demo_onbording/dev/carts/jmeter/basiccheck.jmx /home/dynatrace/$KEPTN_EXAMPLES_DIR/onboarding-carts/jmeter/basiccheck.jmx
