15d6a5dbdd109da283f2bcccc62562c94a4bf980
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-30 18:41:17 -0400

    Update onboard_easytravel_qualitygates.sh

--- resources/easytravel/onboard_easytravel_qualitygates.sh
+++ resources/easytravel/onboard_easytravel_qualitygates.sh
@@ -15,13 +15,13 @@ if [[ -d "easytravel-frontend" ]]; then
     keptn add-resource --project=easytravel --stage=staging --service=easytravel-mongodb --resource=simple_slo.yaml --resourceUri=slo.yaml
     keptn add-resource --project=easytravel --stage=staging --service=easytravel-backend --resource=simple_slo.yaml --resourceUri=slo.yaml
     keptn add-resource --project=easytravel --stage=staging --service=easytravel-frontend --resource=simple_slo.yaml --resourceUri=slo.yaml
-    keptn add-resource --project=easytravel --stage=staging --service=easytravel-wwww --resource=simple_slo.yaml --resourceUri=slo.yaml
+    keptn add-resource --project=easytravel --stage=staging --service=easytravel-nginx --resource=simple_slo.yaml --resourceUri=slo.yaml
 
     echo "Setting up QualityGate to Production"
     keptn add-resource --project=easytravel --stage=production --service=easytravel-mongodb --resource=simple_slo.yaml --resourceUri=slo.yaml
     keptn add-resource --project=easytravel --stage=production --service=easytravel-backend --resource=simple_slo.yaml --resourceUri=slo.yaml
     keptn add-resource --project=easytravel --stage=production --service=easytravel-frontend --resource=simple_slo.yaml --resourceUri=slo.yaml
-    keptn add-resource --project=easytravel --stage=production --service=easytravel-wwww --resource=simple_slo.yaml --resourceUri=slo.yaml    
+    keptn add-resource --project=easytravel --stage=production --service=easytravel-nginx --resource=simple_slo.yaml --resourceUri=slo.yaml    
 
 else 
     echo "The helmcharts for easytravel are not present"
