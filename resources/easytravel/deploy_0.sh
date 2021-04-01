a062e66ae5aac9ab873f044bb8dda20dda55d7fc
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-04-01 10:14:39 -0400

    Update deploy_0.sh

--- resources/easytravel/deploy_0.sh
+++ resources/easytravel/deploy_0.sh
@@ -11,3 +11,5 @@ keptn trigger delivery --project=easytravel --service=easytravel-frontend --imag
 sleep 180
 # Trigger the deployment
 keptn trigger delivery --project=easytravel --service=easytravel-www --image=docker.io/dynatrace/easytravel-nginx --tag=latest --labels=creator=cli
+sleep 180
+keptn trigger delivery --project=easytravel --service=loadgenerator --image=docker.io/dynatrace/easytravel-loadgen --sequence=delivery-direct --tag=latest --labels=creator=cli
