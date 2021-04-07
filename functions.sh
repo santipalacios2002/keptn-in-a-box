cfaa5e351604bc581ad8a1b1759585d79e80296b
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-04-07 17:18:32 -0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -771,7 +771,7 @@ keptndemoUnleash() {
 keptndemoUnleashConfigure() {
   if [ "$keptndemo_unleash_configure" = true ]; then
     printInfoSection "Enable Feature Flags for Unleash and Configure Keptn for it"
-    bashas "cd $KEPTN_EXAMPLES_DIR/onboarding-carts/ &&  bash $KEPTN_IN_A_BOX_DIR/resources/demo/unleash_add_featureflags.sh ${UNLEASH_SERVER}"
+    bashas "cd $KEPTN_EXAMPLES_DIR/onboarding-carts/ && chmod +x $KEPTN_IN_A_BOX_DIR/resources/demo/unleash_add_featureflags.sh && bash $KEPTN_IN_A_BOX_DIR/resources/demo/unleash_add_featureflags.sh ${UNLEASH_SERVER}"
     printInfoSection "No load generation will be created for running the experiment"
     printInfoSection "You can trigger the experiment manually here: https://tutorials.keptn.sh/tutorials/keptn-full-tour-dynatrace-08/#25"
   fi
@@ -820,7 +820,7 @@ keptndemoCartsonboard() {
   if [ "$keptndemo_cartsonboard" = true ]; then
     printInfoSection "Keptn onboarding Carts"
     printInfoSection "Prep Files..."
-    bashas "cd $KEPTN_EXAMPLES_DIR/onboarding-carts/ && bash chmod +x $KEPTN_IN_A_BOX_DIR/resources/demo/prepfiles.sh && bash $KEPTN_IN_A_BOX_DIR/resources/demo/prepfiles.sh"
+    bashas "cd $KEPTN_EXAMPLES_DIR/onboarding-carts/ && chmod +x $KEPTN_IN_A_BOX_DIR/resources/demo/prepfiles.sh && bash $KEPTN_IN_A_BOX_DIR/resources/demo/prepfiles.sh"
     #TODO Parameterize Carts Version.
     bashas "cd $KEPTN_EXAMPLES_DIR/onboarding-carts/ && bash $KEPTN_IN_A_BOX_DIR/resources/demo/onboard_carts.sh" 
     printInfoSection "Keptn onboarding Carts QualityGates"
