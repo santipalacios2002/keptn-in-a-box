06343c82c8bbffaf02c58d5de150d5e0c77678c2
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-04-08 16:34:51 -0400

    added basiccheck

--- resources/easytravel/onboard_easytravel.sh
+++ resources/easytravel/onboard_easytravel.sh
@@ -21,20 +21,31 @@ if [[ -d "easytravel-frontend" ]]; then
     keptn add-resource --project=keptnorders --resource=jmeter/frontend/et-basiccheck.jmx --resourceUri=jmeter/et-basiccheck.jmx
     #keptn add-resource --project=keptnorders --resource=jmeter/jmeter.conf.yaml --resourceUri=jmeter/jmeter.conf.yaml
     echo "load service level jmeter scripts-staging"
-    #keptn add-resource --project=easytravel --service=easytravel-frontend --stage=staging --resource=jmeter/frontend/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
+    keptn add-resource --project=easytravel --service=easytravel-frontend --stage=staging --resource=jmeter/frontend/et-basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
     #keptn add-resource --project=easytravel --service=easytravel-frontend --stage=staging --resource=jmeter/frontend/load.jmx --resourceUri=jmeter/load.jmx
     
-    #keptn add-resource --project=easytravel --service=easytravel-backend --stage=staging --resource=jmeter/backend/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
+    keptn add-resource --project=easytravel --service=easytravel-backend --stage=staging --resource=jmeter/backend/be-basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
     #keptn add-resource --project=easytravel --service=easytravel-backend --stage=staging --resource=jmeter/backend/load.jmx --resourceUri=jmeter/load.jmx
     
+    keptn add-resource --project=easytravel --service=easytravel-www --stage=staging --resource=jmeter/www/www-basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
+    #keptn add-resource --project=easytravel --service=easytravel-backend --stage=staging --resource=jmeter/backend/load.jmx --resourceUri=jmeter/load.jmx
+    
+    keptn add-resource --project=easytravel --service=easytravel-angular --stage=staging --resource=jmeter/angular/angular-basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
+    #keptn add-resource --project=easytravel --service=easytravel-backend --stage=staging --resource=jmeter/backend/load.jmx --resourceUri=jmeter/load.jmx
      
     # add jmeter resources for production
     echo "load service level jmeter scripts-production"
-    #keptn add-resource --project=easytravel --service=easytravel-frontend --stage=production --resource=jmeter/frontend/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
-    #keptn add-resource --project=easytravel --service=easytravel-frontend --stage=production --resource=jmeter/frontend/load.jmx --resourceUri=jmeter/load.jmx
+    keptn add-resource --project=easytravel --service=easytravel-frontend --stage=production --resource=jmeter/frontend/et-basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
+    #keptn add-resource --project=easytravel --service=easytravel-frontend --stage=staging --resource=jmeter/frontend/load.jmx --resourceUri=jmeter/load.jmx
     
-    #keptn add-resource --project=easytravel --service=easytravel-backend --stage=production --resource=jmeter/backend/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
-    #keptn add-resource --project=easytravel --service=easytravel-backend --stage=production --resource=jmeter/backend/load.jmx --resourceUri=jmeter/load.jmx
+    keptn add-resource --project=easytravel --service=easytravel-backend --stage=production --resource=jmeter/backend/be-basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
+    #keptn add-resource --project=easytravel --service=easytravel-backend --stage=staging --resource=jmeter/backend/load.jmx --resourceUri=jmeter/load.jmx
+    
+    keptn add-resource --project=easytravel --service=easytravel-www --stage=production --resource=jmeter/www/www-basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
+    #keptn add-resource --project=easytravel --service=easytravel-backend --stage=staging --resource=jmeter/backend/load.jmx --resourceUri=jmeter/load.jmx
+    
+    keptn add-resource --project=easytravel --service=easytravel-angular --stage=production --resource=jmeter/angular/angular-basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
+    #keptn add-resource --project=easytravel --service=easytravel-backend --stage=staging --resource=jmeter/backend/load.jmx --resourceUri=jmeter/load.jmx
     
     
     # add jmeter config for staging
